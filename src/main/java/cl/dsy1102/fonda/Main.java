e0da0ea6ff6842e8a56ca1f67f85f7af03b3befe
tomcastillos-alt <tom.castillos@duocuc.cl>
2026-09-22 18:58:25 -0300

    Avances tarea

--- src/main/java/cl/dsy1102/fonda/Main.java
+++ src/main/java/cl/dsy1102/fonda/Main.java
@@ -10,6 +10,14 @@ package cl.dsy1102.fonda;
 public class Main {
 
     public static void main(String[] args) {
+
+        BebidaAlcoholica chichaAlcohol = new BebidaAlcoholica("Chicha", 1000, 40, 12.0, false);
+        BebidaAlcoholica piscoSour = new BebidaAlcoholica("Pisco Sour", 500, 25, 18.0, true);
+        BebidaSinAlcohol chichaSana = new BebidaSinAlcohol("Chicha", 1000, 60, 95);
+        BebidaSinAlcohol moteHuesillo = new BebidaSinAlcohol("Mote con Huesillo", 400, 50, 70);
+
+
+
         // TODO 1: instanciar las cuatro bebidas con los datos del enunciado.
         // TODO 2: marcar la bebida alcoholica 'Chicha' con la venta restringida.
         // TODO 3: registrarlas todas en el gestor.
